2968a1dbcc3e3dce879876768f385f16399bcbe6
trongpl94 <namktlc123@gmail.com>
2018-06-21 21:50:06 +0700

    Phantich

---
+++ Phantich.py
@@ -0,0 +1,26 @@
+#  *Snake:
+#   -Attributes:
+#   x: int
+#   y: int
+#   length:int
+#   -Method:
+#   Draw()None
+#   Update()None
+#   Move(pos):None
+#   Eat_food()bool
+#   Eat_self()bool
+#   Eat_wall()bool
+#   *Food:
+#   -Attributes:
+#   x: int
+#   y: int
+#   -Methods:
+#   Draw():None
+#   Snake_eat():Bool
+#   *Game:
+#   -Attributes:
+#   Snake(dict_tail_segment):None
+#   Food(pos):int, random x pos and y pos
+#   -Methods:
+#   Draw-arena():None
+#   Update():None
